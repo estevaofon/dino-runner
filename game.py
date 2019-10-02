e54574e0f06f81ce218c0e208f8fd951aa971090
Estevao Fonseca <estevaopfon@gmail.com>
2019-10-02 11:57:28 -0300

    organizing code

--- game.py
+++ game.py
@@ -10,6 +10,8 @@ import time
 import os
 
 pygame.init()
+pygame.mixer.init()
+
 BASEDIR =  os.path.dirname(os.path.abspath(__file__))
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 400
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
@@ -25,24 +27,22 @@ rex = Animation(rex_sprite_path)
 rex.create_animation(0, 110, 50, 57, "run", duration=120, rows=1, cols=2)
 rex.x = 150
 rex.y = 250
-# Cloud
+
 sprite = pygame.image.load(rex_sprite_path).convert()
-crop_rect = (470, 5, 50, 25)
-cloud1 = sprite.subsurface(crop_rect)
-clouds_positions = [[random.randint(400, 600), random.randint(20,200)] for i in range(10)]
-# Terrain
-crop_rect = (20, 268, 540, 20)
-terrain = sprite.subsurface(crop_rect)
-terrains = [0, 480, 960]
-# Cactus
-crop_rect = (148, 55, 60, 48)
-cactus = sprite.subsurface(crop_rect)
+
+class ScenarioElement():
+    def __init__(self,sprite, crop_rect, coord_list):
+        self.surface = sprite.subsurface(crop_rect)
+        self.coord_list = coord_list
+
 cactus_posx = [600, 900]
-
-
-def s(s0,v0,a,t):
-    return s0 + v0*t + (1/2)*a*t**2 
-
+cactus_list_coord = [[x, 258] for x in cactus_posx]
+cactus_obj = ScenarioElement(sprite, (148, 55, 60, 48), cactus_list_coord)
+terrain_posx = [0, 480, 960]
+terrain_list_coord = [[x, 300] for x in terrain_posx]
+terrain_obj = ScenarioElement(sprite, (20, 268, 540, 20), terrain_list_coord)
+coord_list = [[random.randint(400, 600), random.randint(20,200)] for i in range(10)]
+cloud_obj = ScenarioElement(sprite, (470, 5, 50, 25), coord_list)
 
 jump = False
 gravity = 5
@@ -50,6 +50,21 @@ time1 = time.time()
 velocidade = 100
 jumping = False
 game_speed = 1.3
+pontos = 0
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+game_over = False
+
+sound_path = os.path.join(BASEDIR, "sound", "press.wav")
+sounda= pygame.mixer.Sound(sound_path)
+
+def move_element(scenario_obj, game_speed, game_speed_multiplicator, x_limit, x_start, y_start):
+    for i, coord in enumerate(scenario_obj.coord_list):
+        screen.blit(scenario_obj.surface, coord)
+        #coord x
+        scenario_obj.coord_list[i][0] -= game_speed_multiplicator*game_speed
+        if coord[0] < x_limit:
+            scenario_obj.coord_list[i][0] = x_start
+            scenario_obj.coord_list[i][1] = y_start
 
 if __name__ == '__main__':
     while True:
@@ -61,9 +76,11 @@ if __name__ == '__main__':
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     if rex.y >= 250:
+                        #sounda.play()
                         time1 = time.time()
                         velocidade = -37
                         rex.y = 250
+
                   
         t =  time.time() - time1
         if velocidade < 100 and rex.y <= 250: 
@@ -75,33 +92,34 @@ if __name__ == '__main__':
         surface.fill((255,255,255))
         screen.blit(surface, (0,0))
         
-        for i, terrain_posx in enumerate(terrains):
-            screen.blit(terrain, (terrain_posx,300))
-            terrains[i] -= 3*game_speed
-            if terrain_posx < -500:
-                terrains[i]=960
-
-        for i, cloud_pos in enumerate(clouds_positions):
-            screen.blit(cloud1, cloud_pos)
-            clouds_positions[i][0] -=2
-            if clouds_positions[i][0] < 0:
-                clouds_positions[i][0]= random.randint(800, 1200)
-                clouds_positions[i][1] = random.randint(20, 200)
-
-        for i, posx in enumerate(cactus_posx):
-            cactus_posx[i] -= 5*game_speed
-            cactus_rect = pygame.Rect(cactus_posx[i]+10, 270, 50, 40)
-            pygame.draw.rect(screen, (0,255,0), cactus_rect)
-            screen.blit(cactus, (posx,258))
-            if posx < 100:
-                cactus_posx[i] = random.randint(800, 1500)
-                 
+        move_element(terrain_obj, game_speed, 3, -500, 960, terrain_obj.coord_list[0][1])
+        move_element(cloud_obj, game_speed, 1, 0, random.randint(800, 1200), random.randint(20, 200))
         rex_rect = pygame.Rect(rex.x+2, rex.y, 40, 60)
         pygame.draw.rect(screen, (255,0,0), rex_rect)
-        if rex_rect.colliderect(cactus_rect):
-            print("Colidiu")
 
 
+        #for i, coord in enumerate(cactus_obj.coord_list):
+        #    cactus_obj.coord_list[i][0] -= 5*game_speed
+        #    cactus_rect = pygame.Rect(cactus_obj.coord_list[i][0]+10, 270, 50, 40)
+        #    pygame.draw.rect(screen, (0,255,0), cactus_rect)
+        #    screen.blit(cactus_obj.surface, coord)
+        #    if coord[0] < 100:
+        #        cactus_obj.coord_list[i][0] = random.randint(800, 1500)
+        #
+        #    if rex_rect.colliderect(cactus_rect):
+        #        print("Colidiu")
+        #        game_over = True
+
+        if game_over:
+            textsurface_over = myfont.render(f'Game Over', False, (0, 0, 0))
+            screen.blit(textsurface_over,(350,200))
+        if not game_over:
+            pontos += 0.5
+        
+        move_element(cactus_obj, game_speed, 4, 100, random.randint(800, 1500), cactus_obj.coord_list[0][1])
+        print(f"pontos: {pontos}")
+        textsurface = myfont.render(f'{pontos:.0f}', False, (0, 0, 0))
+        screen.blit(textsurface,(700,0))
         screen.blit(rex.update_surface(), (rex.x, rex.y))
         rex.run("run")
         #pygame.display.flip()
